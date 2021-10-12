8dc1addc8072efd5af9f22aa018fa5ea785ebd17
Gregory Barratt <barrattg@hotmail.co.uk>
2021-10-12 15:20:30 +0100

    added take out copy/ return copy(stub)

---
+++ src/main/java/org/softwire/training/bookish/commands/ReturnCopy.java
@@ -0,0 +1,29 @@
+package org.softwire.training.bookish.commands;
+
+import org.jdbi.v3.core.Jdbi;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class ReturnCopy implements Command {
+
+    @Override
+    public void Execute(String input, Jdbi jdbi) throws ParseException {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Please Enter LoanID: ");
+        String LoanID = myObj.nextLine();
+
+        Object o = jdbi.withHandle(handle ->
+                handle.createUpdate("UPDATE loans SET ReturnedDate = '2002-08-03' WHERE Loans.ID =:ID"
+
+
+                )
+                        .bind("ID", LoanID)
+                        .execute()
+        );
+        System.out.println(o);
+        }
+    }
+
