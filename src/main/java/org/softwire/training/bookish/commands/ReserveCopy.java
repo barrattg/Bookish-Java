d6d02393352ffc20ec98815cb1f3494162ec2dbf
Gregory Barratt <barrattg@hotmail.co.uk>
2021-10-15 16:00:44 +0100

    added copy reservation

--- src/main/java/org/softwire/training/bookish/commands/ReserveCopy.java
+++ src/main/java/org/softwire/training/bookish/commands/ReserveCopy.java
@@ -2,9 +2,11 @@ package org.softwire.training.bookish.commands;
 import org.jdbi.v3.core.Jdbi;
 import org.softwire.training.bookish.models.database.Book;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Scanner;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+import java.util.*;
 
 public class ReserveCopy implements Command {
     //checks if any copy is available then takes a copy ID and
@@ -13,35 +15,47 @@ public class ReserveCopy implements Command {
 
     @Override
     public void Execute(String input, Jdbi jdbi) {
-        //todo copy available
+
         Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter Book Name to be reserved: ");
-        String bookName = myObj.nextLine();
+        System.out.println("Enter Book ID to be reserved: ");
+        String bookID = myObj.nextLine();
         System.out.println("Enter UserID: ");
         String UserID = myObj.nextLine();
-        //todo change copy class to have copyID /checkout and remove expected return
+
+        String LoanedDate = DateTimeFormatter.ofPattern("yyyy-MM-dd").format(java.time.LocalDate.now());
+
+
          try {
 
              Object o = jdbi.withHandle(handle ->
 
-                     handle.createUpdate("INSERT INTO loans (UserID, CopyID) SELECT :UserID, copies.ID" +
-                                     " FROM copies " +
-                                     " JOIN books ON books.ID = copies.BookID " +
-                                     " LEFT OUTER JOIN loans ON copies.ID = loans.CopyID " +
-                                     " WHERE Books.Name = :Name AND (loans.UserID IS null or" +
-                                     " loans.ReturnedDate is not null) " +
-                                     " limit 1")
+                     handle.createUpdate("INSERT INTO loans (UserID, CopyID, LoanedDate) SELECT :UserID, copies.ID, :LoanedDate " +
+                     "FROM copies "+
+                     "JOIN books ON books.ID = copies.BookID "+
+                     "LEFT OUTER JOIN loans ON copies.ID = loans.CopyID " +
+                     "WHERE Books.ID =:ID AND ( loans.CopyID IS null OR copies.ID NOT IN (Select copies.ID FROM loans " +
+                     "JOIN copies ON loans.CopyID= copies.ID " +
+                     "WHERE ReturnedDate IS NULL)) " +
+                     "limit 1; ")
 
-                             .bind("Name", bookName)
+                             .bind("ID", bookID)
                              .bind("UserID", UserID)
+                             .bind("LoanedDate", LoanedDate)
                              .execute()
 
              );
              if (o.toString().equals("0") ) {
-                 System.out.println("There were no copies available of " + bookName + " sorry");
+                 System.out.println("There were no copies available of " + bookID + " sorry, if you would like to" +
+                         "reserve a copy, please enter a date yyyy-mm-dd");
+
+
+                 Object p = jdbi.withHandle(handle ->
+
+                         handle.createQuery("SELECT FROM LOANS (CopyID, LoanedDate) ORDER BY LoanedDate limit 1"));
+
              }
              else {
-                 System.out.println(bookName + " added to loans table for User " + UserID);
+                 System.out.println(bookID + " added to loans table for User " + UserID);
              }
          }
          catch (Exception e){
