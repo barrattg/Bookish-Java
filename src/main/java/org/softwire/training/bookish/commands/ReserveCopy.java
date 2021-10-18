ad42b02808f3b356ac301be7c55e776bc05a75c6
Gregory Barratt <barrattg@hotmail.co.uk>
2021-10-18 13:35:51 +0100

    completed logic for reserve system

--- src/main/java/org/softwire/training/bookish/commands/ReserveCopy.java
+++ src/main/java/org/softwire/training/bookish/commands/ReserveCopy.java
@@ -4,9 +4,11 @@ import org.softwire.training.bookish.models.database.Book;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class ReserveCopy implements Command {
     //checks if any copy is available then takes a copy ID and
@@ -45,15 +47,58 @@ public class ReserveCopy implements Command {
 
              );
              if (o.toString().equals("0") ) {
-                 System.out.println("There were no copies available of " + bookID + " sorry, if you would like to" +
-                         "reserve a copy, please enter a date yyyy-mm-dd");
+                 System.out.println("There were no copies available of " + bookID + " sorry");
 
 
-                 Object p = jdbi.withHandle(handle ->
 
-                         handle.createQuery("SELECT FROM LOANS (CopyID, LoanedDate) ORDER BY LoanedDate limit 1"));
+                 String date = jdbi.withHandle(handle ->
 
+                         handle.createQuery("SELECT LoanedDate FROM LOANS " +
+                                 "JOIN copies ON loans.CopyID = copies.ID " +
+                                 "JOIN books ON books.ID = copies.BookID " +
+                                 "WHERE Books.ID =:ID AND " +
+                                 "(loanedDate is NOT NULL and ReturnedDate is NULL) order by LoanedDate asc Limit 1 ;" )
+                                 //.map((rs, ctx)-> rs.getString("LoanedDate"))
+                                 .bind("ID", bookID)
+                                 .mapTo(String.class)
+                                 .list().get(0)
+                                 );
+
+
+                 LocalDate futureDate = LocalDate.parse(date).plusDays(7);
+
+                 System.out.println("Would you like to reserve the first available copy on the " + futureDate + " ? yes/no");
+
+                 String reserve = myObj.nextLine();
+
+
+                 if(reserve.equals("yes")){
+
+                     Object q = jdbi.withHandle(handle ->
+
+                             handle.createUpdate("INSERT INTO loans (UserID, CopyID, LoanedDate) SELECT :UserID, copies.ID, :LoanedDate " +
+                             "FROM copies "+
+                             "JOIN books ON books.ID = copies.BookID "+
+                             "LEFT OUTER JOIN loans ON copies.ID = loans.CopyID " +
+                             "WHERE Books.ID =:ID limit 1"
+                             )
+                                     .bind("ID", bookID)
+                                     .bind("UserID", UserID)
+                                     .bind("LoanedDate", futureDate)
+                                     .execute()
+                     );
+                     System.out.println("Copy " + bookID + " was reserved on " +futureDate + " for user " + UserID);
+
+                 }
+                 else{
+                     System.out.println("Please contact a librarian to reserve the book for another date ");
+
+                 }
              }
+
+
+
+
              else {
                  System.out.println(bookID + " added to loans table for User " + UserID);
              }
